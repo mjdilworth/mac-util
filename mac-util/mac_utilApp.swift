20cd0867feff67dbccf68a5b33aac8e1fad03fac
Mike Dilworth <mike@dilworth.uk>
2025-08-23 20:36:16 +0100

    Initial Commit

---
+++ mac-util/mac_utilApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_utilApp.swift
+//  mac-util
+//
+//  Created by Michael Dilworth on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct mac_utilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
